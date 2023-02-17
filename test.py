f473ce3d4aaca5e839c29ab3f7afd9a6444dfb1a
alihanks <j.ali.hanks@gmail.com>
2023-02-17 14:21:18 -0800

    new examples

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import random
 import time
+import argparse
+import sys
 
+print(sys.argv)
 
+start_time = time.time()
+run_time = int(sys.argv[1])
+itime = start_time
 
-while True:
+while itime < (start_time + run_time):
   itime = time.time()
   idata = random.random()
   print(itime, idata)
